f0463a780e004cf6069555498e2ce32dfa290422
Onesinus Tamba <onesinus231@gmail.com>
2024-03-27 00:26:32 +0700

    Initial commit

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+// src/pages/LoginPage.js
+
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {/* Add your login form here */}
+    </div>
+  );
+}
+
+export default LoginPage;
